76680232d38e9efe7a378ba294526c1dc969293c
Pablo Goulart <pablogoulart02@gmail.com>
2023-10-15 20:22:19 -0300

    Exercicio de variaveis e comentários

---
+++ Exercicios02/JS/base.js
@@ -0,0 +1,17 @@
+//Comentário por linha
+
+/*
+Comentário
+em
+várias
+linhas
+*/
+
+let nome = prompt('Digite seu nome: ');
+
+alert('Olá, ' + nome + ' seja bem-vindo!');
+
+let númeroPositivo = false;
+
+console.log(typeof númeroPositivo);
+console.log(typeof nome);
